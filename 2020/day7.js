da0d4d316e7127f5811210f2f7134d3c4e11d440
Jacob Heider <jacob.heider@candor-usa.com>
2020-12-16 12:48:40 -0500

    Day 7 solution

---
+++ 2020/day7.js
@@ -0,0 +1,26 @@
+const data = require('./day7.json')
+
+const tree_bags = color => {
+  let contains = new Set([color])
+  let size
+  do {
+    size = contains.size
+    contains.forEach(color => data.forEach(d => { if (d[1].match(new RegExp(color))) contains.add(d[0].slice(0, -1)) } ))
+  } while (size !== contains.size)
+  contains.delete(color)
+  return contains
+}
+
+console.log('Part 1:', tree_bags('shiny gold').size)
+
+const contains = color => {
+  const contents = data.find(d => d[0] === `${color} bags`)[1]
+  if (contents === 'no other bags.') { return 0 }
+  return contents.split(', ').map(c => {
+    const num = +c.split(' ')[0]
+    const color = c.match(/[0-9]+ (.*) bag.*$/)[1]
+    return num + num * contains(color)
+  }).reduce((a, b) => a + b)
+}
+
+console.log('Part 2:', contains('shiny gold'))
