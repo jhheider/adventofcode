9f9ec66f19a68f10807883ea6f17198e6fdb8390
Jacob Heider <jacob.heider@candor-usa.com>
2020-12-16 12:48:25 -0500

    Day 1 solution

---
+++ 2020/day1.js
@@ -0,0 +1,25 @@
+const { test, input } = require('./day1.json')
+
+const find_and_multiply2 = data => {
+  for (let x = 0; x < data.length - 1; x++) {
+    for (let y = x + 1; y < data.length; y++) {
+      if (data[x] + data[y] === 2020) { return [data[x], data[y], data[x] * data[y]] }
+    }
+  }
+}
+
+const find_and_multiply3 = data => {
+  for (let x = 0; x < data.length - 2; x++) {
+    for (let y = x + 1; y < data.length - 1; y++) {
+      for (let z = y + 1; z < data.length - 1; z++) {
+        if (data[x] + data[y] + data[z] === 2020) { return [data[x], data[y], data[z], data[x] * data[y] * data[z]] }
+      }
+    }
+  }
+}
+
+console.log('Test 1:', find_and_multiply2(test))
+console.log('Part 1:', find_and_multiply2(input))
+
+console.log('Test 2:', find_and_multiply3(test))
+console.log('Part 2:', find_and_multiply3(input))
