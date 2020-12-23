0c1207268e11447e43dec0ce168a4a2930ecaf2f
Jacob Heider <jacob.heider@candor-usa.com>
2020-12-23 02:04:25 -0500

    Day 22 solution

---
+++ 2020/day22.js
@@ -0,0 +1,41 @@
+const _ = require('lodash')
+const { test, input } = require('./day22.json')
+
+const combat = input => {
+  const [a, b] = [[...input[0]], [...input[1]]]
+  while (a.length > 0 && b.length > 0) {
+    const A = a.shift()
+    const B = b.shift()
+    A > B ? a.push(A, B) : b.push(B, A)
+  }
+  const s = score(a.length > 0 ? a : b)
+  return [a, b, s]
+}
+
+const score = input => input.reduce((s, v, i, a) => s + v * (a.length - i), 0)
+
+console.log('Test 1:', combat(test)[2])
+console.log('Part 1:', combat(input)[2])
+
+const recursive_combat = _.memoize(input => {
+  const [a, b] = [[...input[0]], [...input[1]]]
+  const seen = new Set()
+  while (a.length > 0 && b.length > 0) {
+    const decks = [a.join(':'), b.join(':')].join(',')
+    if (seen.has(decks)) return [a, b, 'Player 1', score(a)]
+    seen.add(decks)
+    const A = a.shift()
+    const B = b.shift()
+    if (a.length >= A && b.length >= B) {
+      recursive_combat([a.slice(0, A), b.slice(0, B)])[2] === 'Player 1' ? a.push(A, B) : b.push(B, A)
+    } else {
+      A > B ? a.push(A, B) : b.push(B, A)
+    }
+  }
+  const s = (a.length > 0 ? a : b).reduce((s, v, i, arr) => s + v * (arr.length - i), 0)
+  const winner = a.length > 0 ? 'Player 1' : 'Player 2'
+  return [a, b, winner, s]
+})
+
+console.log('Test 2:', recursive_combat(test)[3])
+console.log('Part 2:', recursive_combat(input)[3])
