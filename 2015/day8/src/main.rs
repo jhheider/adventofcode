87dae182c23074a0f77ae2b6a5d555f17fe8f01f
Jacob Heider <jacob@myhealthily.com>
2021-06-02 20:07:24 -0400

    Not bad.

---
+++ 2015/day8/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs;
+use regex::Regex;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let part1 = input.lines().fold(0, |a, b| { a + decoded_savings(b) });
+
+    println!("Part 1: {} characters saved.", part1);
+
+    let part2 = input.lines().fold(0, |a, b| { a + encoded_gain(b) });
+
+    println!("Part 2: {} characters added.", part2);
+}
+
+fn decoded_savings(s: &str) -> usize {
+    let escape = Regex::new(r#"\\([\\"])|\\x([0-9a-zA-Z]{2})"#).unwrap();
+    let memory = escape.replace_all(&s, "a").to_string();
+
+    s.len() - memory.len() + 2
+}
+
+fn encoded_gain(s: &str) -> usize { s.matches("\\").count() + s.matches("\"").count() + 2 }
---
+++ 2015/day9/src/main.rs
@@ -0,0 +1,69 @@
+use std::{
+        fs,
+        cmp::{min, max},
+        collections::{HashMap, HashSet},
+};
+use regex::Regex;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let test = r"London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141";
+
+    let test = solve(&test);
+    assert_eq!(test, (605, 982));
+
+    let part1 = solve(&input);
+    assert_eq!(part1, (251, 898));
+
+    println!("test: {} - {}", test.0, test.1);
+    println!("Part 1: {} - {}", part1.0, part1.1);
+}
+
+fn solve(list: &str) -> (u32, u32) {
+    let mut edges = HashMap::new();
+    let mut nodes = HashSet::new();
+
+    for l in list.lines() {
+        let r = Regex::new(r"(.*) to (.*) = ([0-9]+)").unwrap();
+        let caps = r.captures(&l).unwrap();
+
+        let a = caps.get(1).unwrap().as_str();
+        let b = caps.get(2).unwrap().as_str();
+        let d = caps.get(3).unwrap().as_str().parse().unwrap();
+
+        edges.insert((a, b), d);
+        edges.insert((b, a), d);
+        edges.insert(("#", a), 0);
+        edges.insert(("#", b), 0);
+        nodes.insert(a);
+        nodes.insert(b);
+    }
+    solve_rec(&nodes, &edges, "#", HashSet::new())
+}
+
+fn solve_rec(nodes: &HashSet<&str>, edges: &HashMap<(&str, &str), u32>, start: &str, visited: HashSet<&str>) -> (u32, u32) {
+    if visited.len() == nodes.len() { return (0, 0) }
+
+    let mut min0 = u32::MAX;
+    let mut max0 = 0;
+
+    for node in nodes.iter() {
+        if visited.contains(node) { continue }
+
+        let mut visited = visited.clone();
+
+        visited.insert(node);
+
+        let t = solve_rec(nodes, edges, node, visited);
+        let t_min = t.0 + edges.get(&(start, *node)).unwrap();
+        min0 = min(t_min, min0);
+
+        let t_max = t.1 + edges.get(&(start, *node)).unwrap();
+        max0 = max(t_max, max0);
+    }
+
+    (min0, max0)
+}
