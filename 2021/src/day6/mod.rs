d45d216de320b342e5097ecd0299085e0fdff7af
Jacob Heider <jacob@myhealthily.com>
2021-12-07 10:51:29 -0500

    2021.6 and 7

--- 2021/src/day6/mod.rs
+++ 2021/src/day6/mod.rs
@@ -1,1 +1,52 @@
-pub fn main() {}
+use std::fs;
+
+pub fn main() {
+  let test = "3,4,3,1,2".split(',').map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+  let input = fs::read_to_string("data/day6.txt").unwrap().split(',').map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+
+  let test1a = run(&test, 18);
+  assert_eq!(test1a, 26);
+  println!("Day 6: Test 1a: after 18 days, there are {} fish", test1a);
+
+  let test1b = run(&test, 80);
+  assert_eq!(test1b, 5934);
+  println!("Day 6: Test 1a: after 80 days, there are {} fish", test1b);
+
+  let part1 = run(&input, 80);
+  assert_eq!(part1, 377263);
+  println!("Day 6: Part 1: after 80 days, there are {} fish", part1);
+
+  let test2 = run(&test, 256);
+  assert_eq!(test2, 26984457539);
+  println!("Day 6: Test 2: after 256 days, there are {} fish", test2);
+
+  let part2 = run(&input, 256);
+  assert_eq!(part2, 1695929023803);
+  println!("Day 6: Part 2: after 256 days, there are {} fish", part2);
+}
+
+fn run(input: &[u64], days: u64) -> u64 {
+  let mut state: Vec<u64> = vec![0; 9];
+
+  for i in input {
+    state[*i as usize] += 1;
+  }
+
+  for _ in 0..days {
+    let mut next_state = vec![0; 9];
+
+    for day in 0..=8 {
+      match day {
+        0 => {
+          next_state[6] = state[0];
+          next_state[8] = state[0];
+        },
+        _ => {
+          next_state[day - 1] += state[day];
+        }
+      }
+    }
+    state = next_state
+  }
+  state.iter().sum()
+}
--- 2021/src/day7/mod.rs
+++ 2021/src/day7/mod.rs
@@ -1,1 +1,52 @@
-pub fn main() {}
+use std::fs;
+use num::abs;
+
+pub fn main() {
+  let test = "16,1,2,0,4,2,7,1,2,14"
+    .split(',')
+    .map(|s| s.parse::<i32>().unwrap())
+    .collect::<Vec<i32>>();
+  let input = fs::read_to_string("data/day7.txt")
+    .unwrap()
+    .split(',')
+    .map(|s| s.parse::<i32>().unwrap())
+    .collect::<Vec<i32>>();
+
+  let test1 = least_fuel(&test, false);
+  assert_eq!(test1, (2, 37));
+  println!("Day 7: Test 1: Least fuel ({}) used to reach postition {}", test1.1, test1.0);
+
+  let part1 = least_fuel(&input, false);
+  assert_eq!(part1, (342, 325528));
+  println!("Day 7: Part 1: Least fuel ({}) used to reach postition {}", part1.1, part1.0);
+
+  let test2 = least_fuel(&test, true);
+  assert_eq!(test2, (5, 168));
+  println!("Day 7: Test 2: Least fuel ({}) used to reach postition {}", test2.1, test2.0);
+
+  let part2 = least_fuel(&input, true);
+  assert_eq!(part2, (460, 85015836));
+  println!("Day 7: Part 2: Least fuel ({}) used to reach postition {}", part2.1, part2.0);
+}
+
+fn least_fuel(input: &[i32], part2: bool) -> (i32, i32) {
+  let lowest = input.iter().min().unwrap();
+  let highest = input.iter().max().unwrap();
+
+  let mut output = (-1, i32::MAX);
+
+  for position in *lowest..=*highest {
+    let cost = input.iter()
+      .fold(0, |acc, el| {
+        if part2 {
+          let step = abs(el - position);
+          return acc + step * (step + 1) / 2
+        }
+        acc + abs(el - position)
+      });
+
+    if cost < output.1 { output = (position, cost); }
+  }
+
+  output
+}
