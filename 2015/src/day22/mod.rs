dac96fdd0f57f3bd604674d43fe21d12aa0e8f0f
Jacob Heider <jacob@tea.xyz>
2022-12-05 19:26:31 -0400

    2015.22

--- 2015/src/day22/mod.rs
+++ 2015/src/day22/mod.rs
@@ -1,159 +1,191 @@
-// use std::cmp::min;
-// use strum::IntoEnumIterator;
-// use strum_macros::EnumIter;
+use std::collections::VecDeque;
 
-// #[derive(EnumIter)]
-// enum Spell {
-//     MagicMissile,
-//     Drain,
-//     Shield,
-//     Poison,
-//     Recharge,
-// }
-
-// impl Spell {
-//     fn cost(&self) -> i32 {
-//         match *self {
-//             Spell::MagicMissile => 53,
-//             Spell::Drain => 73,
-//             Spell::Shield => 113,
-//             Spell::Poison => 173,
-//             Spell::Recharge => 239,
-//         }
-//     }
-// }
-
-// struct GameState {
-//     pc_hp: i32,
-//     pc_mana: i32,
-//     mana_spent: i32,
-//     boss_hp: i32,
-//     boss_dmg: i32,
-//     shield: i32,
-//     poison: i32,
-//     recharge: i32,
-//     status: BattleState,
-// }
-
-// #[derive(PartialEq)]
-// enum BattleState {
-//     won,
-//     lost,
-//     ongoing,
-// }
-
-// impl GameState {
-//     fn start_turn(&mut self) {
-//         if self.poison > 0 {
-//             self.boss_hp -= 3;
-//             self.poison -= 1;
-//             if self.boss_hp <= 0 {
-//                 self.status = BattleState::lost
-//             }
-//         }
-//         if self.recharge > 0 {
-//             self.pc_mana += 101;
-//             self.recharge -= 1;
-//         }
-//     }
-
-//     fn pc_turn(&mut self, spell: Spell) {
-//         self.start_turn();
-
-//         if spell.cost() > self.pc_mana {
-//             self.status = BattleState::lost;
-//             self.end_turn();
-//             return;
-//         }
-
-//         self.mana_spent += spell.cost();
-//         self.pc_mana -= spell.cost();
-
-//         match spell {
-//             Spell::MagicMissile => self.boss_hp -= 4,
-//             Spell::Drain => {
-//                 self.pc_hp += 2;
-//                 self.boss_hp -= 2;
-//             }
-//             Spell::Shield => {
-//                 if self.shield > 0 {
-//                     self.status = BattleState::lost
-//                 }
-//                 self.shield = 7;
-//             }
-//             Spell::Poison => {
-//                 if self.poison > 0 {
-//                     self.status = BattleState::lost
-//                 }
-//                 self.poison = 6;
-//             }
-//             Spell::Recharge => {
-//                 if self.recharge > 0 {
-//                     self.status = BattleState::lost
-//                 }
-//                 self.recharge = 5;
-//             }
-//         }
-
-//         self.end_turn();
-//     }
-
-//     fn boss_turn(&mut self) {
-//         self.start_turn();
-//         if self.shield > 0 {
-//             self.pc_hp -= min(1, self.boss_dmg - 7);
-//         } else {
-//             self.pc_hp -= self.boss_dmg;
-//         }
-//         self.end_turn();
-//     }
-
-//     fn end_turn(&mut self) {
-//         if self.shield > 0 {
-//             self.shield -= 1
-//         }
-//         if self.status == BattleState::ongoing {
-//             if self.pc_hp <= 0 {
-//                 self.status = BattleState::lost
-//             }
-//             if self.boss_hp <= 0 {
-//                 self.status = BattleState::won
-//             }
-//         }
-//     }
-// }
-
-pub fn main() {
-    //     let test = GameState {
-    //         pc_hp: 10,
-    //         pc_mana: 250,
-    //         mana_spent: 0,
-    //         boss_hp: 13,
-    //         boss_dmg: 8,
-    //         shield: 0,
-    //         poison: 0,
-    //         recharge: 0,
-    //         status: BattleState::ongoing,
-    //     };
-
-    //     let test1 = play_game(&test);
-    //     assert_eq!(test1, (4, 226));
-    //     println!(
-    //         "Day 22: Test 1: PC wins after {} rounds using {} mana",
-    //         test1.0, test1.1
-    //     );
+#[derive(Clone, Debug)]
+struct GameState {
+    hp: isize,
+    mana: isize,
+    boss_hp: isize,
+    boss_damage: usize,
+    shield: usize,
+    poison: usize,
+    recharge: usize,
+    mana_spent: usize,
+    is_hard: bool,
 }
 
-// fn play_game(initial_state: &GameState) -> (i32, i32) {
-//     let mut state = initial_state.clone();
-//     let best_win = i32::MAX;
-//     for round in 1.. {
-//         match round % 2 {
-//             1 => unimplemented!(),
-//             0 => unimplemented!(),
-//             _ => panic!(),
-//         }
-//     }
+#[derive(Clone, Debug)]
+struct Spell {
+    cost: usize,
+    effect: Effect,
+}
 
-//     (0, 0)
-// }
+#[derive(Clone, Debug)]
+enum Effect {
+    MagicMissile,
+    Drain,
+    Shield,
+    Poison,
+    Recharge,
+}
+
+impl GameState {
+    fn new(hp: isize, mana: isize, boss_hp: isize, boss_damage: usize, is_hard: bool) -> Self {
+        GameState {
+            hp,
+            mana,
+            boss_hp,
+            boss_damage,
+            shield: 0,
+            poison: 0,
+            recharge: 0,
+            mana_spent: 0,
+            is_hard,
+        }
+    }
+
+    fn apply_effects(&mut self) {
+        if self.shield > 0 {
+            self.shield -= 1;
+        }
+        if self.poison > 0 {
+            self.boss_hp -= 3;
+            self.poison -= 1;
+        }
+        if self.recharge > 0 {
+            self.mana += 101;
+            self.recharge -= 1;
+        }
+    }
+
+    fn apply_spell(&mut self, spell: &Spell) {
+        self.mana -= spell.cost as isize;
+        self.mana_spent += spell.cost;
+        match spell.effect {
+            Effect::MagicMissile => self.boss_hp -= 4,
+            Effect::Drain => {
+                self.boss_hp -= 2;
+                self.hp += 2;
+            }
+            Effect::Shield => self.shield = 6,
+            Effect::Poison => self.poison = 6,
+            Effect::Recharge => self.recharge = 5,
+        }
+    }
+
+    fn is_won(&self) -> bool {
+        self.boss_hp <= 0
+    }
+
+    fn is_lost(&self) -> bool {
+        self.hp <= 0 || self.mana < 0
+    }
+
+    fn is_over(&self) -> bool {
+        self.is_won() || self.is_lost()
+    }
+
+    fn is_valid(&self) -> bool {
+        self.mana >= (53 - 101) && self.boss_hp > 0 && self.hp > 0
+    }
+
+    fn boss_turn(&mut self) {
+        self.apply_effects();
+        if self.shield > 0 {
+            self.hp -= self.boss_damage as isize - 7
+        } else {
+            self.hp -= self.boss_damage as isize
+        };
+    }
+
+    fn player_turn(&mut self, spell: &Spell) {
+        if self.is_hard {
+            self.hp -= 1;
+            if self.hp <= 0 {
+                return;
+            }
+        }
+        self.apply_effects();
+        self.apply_spell(spell);
+        if !self.is_over() {
+            self.boss_turn();
+        }
+    }
+
+    fn play(&mut self, spell: &Spell) -> bool {
+        self.player_turn(spell);
+        self.is_won()
+    }
+}
+
+impl Spell {
+    fn new(cost: usize, effect: Effect) -> Self {
+        Spell { cost, effect }
+    }
+}
+
+pub fn main() {
+    let test = GameState::new(10, 250, 13, 8, false);
+    let test1 = min_mana(test);
+    assert_eq!(test1, 226);
+    println!("Day 22 Test 1: {}", test1);
+
+    let input = GameState::new(50, 500, 58, 9, false);
+    let part1 = min_mana(input);
+    assert_eq!(part1, 1269);
+    println!("Day 22 Part 1: {}", part1);
+
+    let input2 = GameState::new(50, 500, 58, 9, true);
+    let part2 = min_mana(input2);
+    assert_eq!(part2, 1309);
+    println!("Day 22 Part 2: {}", part2);
+}
+
+fn min_mana(start: GameState) -> usize {
+    let mut min_mana = usize::max_value();
+    let spells = vec![
+        Spell::new(53, Effect::MagicMissile),
+        Spell::new(73, Effect::Drain),
+        Spell::new(113, Effect::Shield),
+        Spell::new(173, Effect::Poison),
+        Spell::new(229, Effect::Recharge),
+    ];
+    let mut queue = VecDeque::new();
+    queue.push_back(start);
+    while let Some(game) = queue.pop_front() {
+        for spell in &spells {
+            let max_mana = if game.recharge > 0 {
+                game.mana + 101
+            } else {
+                game.mana
+            };
+            if spell.cost > max_mana as usize {
+                continue;
+            }
+            match spell.effect {
+                Effect::Shield => {
+                    if game.shield > 1 {
+                        continue;
+                    }
+                }
+                Effect::Poison => {
+                    if game.poison > 1 {
+                        continue;
+                    }
+                }
+                Effect::Recharge => {
+                    if game.recharge > 1 {
+                        continue;
+                    }
+                }
+                _ => (),
+            }
+            let mut game = game.clone();
+            if game.play(spell) {
+                min_mana = min_mana.min(game.mana_spent);
+            } else if game.is_valid() && game.mana_spent < min_mana {
+                queue.push_back(game);
+            }
+        }
+    }
+    min_mana
+}
