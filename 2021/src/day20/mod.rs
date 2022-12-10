9d3fac123f6c5201a6d9653fe2731c84d8120be8
Jacob Heider <jacob@tea.xyz>
2022-12-09 20:38:38 -0400

    2021.20

--- 2021/src/day20/mod.rs
+++ 2021/src/day20/mod.rs
@@ -1,1 +1,168 @@
-pub fn main() {}
+use std::{collections::HashSet, fs};
+
+const TEST: &str = r"..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##
+#..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###
+.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#.
+.#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#.....
+.#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#..
+...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.....
+..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###";
+
+struct Algorithm {
+  algorithm: Vec<bool>,
+}
+
+struct Image {
+  top: isize,
+  left: isize,
+  right: isize,
+  bottom: isize,
+  image: HashSet<(isize, isize)>,
+}
+
+impl Algorithm {
+  fn new(algorithm: Vec<&str>) -> Self {
+    let algorithm = algorithm
+      .iter()
+      .map(|line| line.chars().map(|c| c == '#').collect::<Vec<bool>>())
+      .into_iter()
+      .flatten()
+      .collect();
+    Self { algorithm }
+  }
+
+  fn lookup(&self, index: usize) -> bool {
+    self.algorithm[index]
+  }
+}
+
+impl Image {
+  fn new(input: &str) -> Self {
+    let mut image = HashSet::new();
+    let top = 0;
+    let left = 0;
+    let mut right = 0;
+    let mut bottom = 0;
+    for (y, line) in input.lines().enumerate() {
+      for (x, c) in line.chars().enumerate() {
+        if c == '#' {
+          image.insert((x as isize, y as isize));
+          if x as isize > right {
+            right = x as isize;
+          }
+          if y as isize > bottom {
+            bottom = y as isize;
+          }
+        }
+      }
+    }
+    Self {
+      top,
+      left,
+      right,
+      bottom,
+      image,
+    }
+  }
+
+  fn enhance(&mut self, algorithm: &Algorithm) {
+    let mut new_image = HashSet::new();
+    for y in self.top - 1..=self.bottom + 1 {
+      for x in self.left - 1..=self.right + 1 {
+        let mut index = 0;
+        for (dx, dy) in &[
+          (x - 1, y - 1),
+          (x, y - 1),
+          (x + 1, y - 1),
+          (x - 1, y),
+          (x, y),
+          (x + 1, y),
+          (x - 1, y + 1),
+          (x, y + 1),
+          (x + 1, y + 1),
+        ] {
+          index <<= 1;
+          if self.image.get(&(*dx, *dy)).is_some() {
+            index += 1;
+          }
+        }
+        if algorithm.lookup(index) {
+          new_image.insert((x, y));
+        }
+      }
+    }
+    // Special case for "toggle" rules
+    if algorithm.lookup(0) && !algorithm.lookup(511) {
+      if self.image.get(&(self.left - 1, self.top - 1)).is_none() {
+        for i in self.left - 3..=self.right + 3 {
+          new_image.insert((i, self.top - 3));
+          new_image.insert((i, self.top - 2));
+          new_image.insert((i, self.bottom + 2));
+          new_image.insert((i, self.bottom + 3));
+          new_image.insert((self.left - 3, i));
+          new_image.insert((self.left - 2, i));
+          new_image.insert((self.right + 2, i));
+          new_image.insert((self.right + 3, i));
+        }
+      } else {
+        for i in self.left - 2..=self.right + 2 {
+          new_image.remove(&(i, self.top - 2));
+          new_image.remove(&(i, self.bottom + 2));
+          new_image.remove(&(self.left - 2, i));
+          new_image.remove(&(self.right + 2, i));
+        }
+      }
+    }
+    self.image = new_image;
+    self.top -= 1;
+    self.left -= 1;
+    self.right += 1;
+    self.bottom += 1;
+  }
+
+  fn lit(&self) -> usize {
+    self.image.len()
+  }
+
+  fn run(&mut self, algorithm: &Algorithm, iterations: usize) {
+    for _ in 0..iterations {
+      self.enhance(algorithm);
+    }
+  }
+}
+
+pub fn main() {
+  let mut test1 = parse(TEST);
+  test1.1.run(&test1.0, 2);
+  assert_eq!(test1.1.lit(), 35);
+  println!("Day 20, test 1: {}", test1.1.lit());
+
+  let mut input1 = parse(&fs::read_to_string("data/day20.txt").unwrap());
+  input1.1.run(&input1.0, 2);
+  assert_eq!(input1.1.lit(), 4873);
+  println!("Day 20, part 1: {}", input1.1.lit());
+
+  let mut test2 = parse(TEST);
+  test2.1.run(&test2.0, 50);
+  assert_eq!(test2.1.lit(), 3351);
+  println!("Day 20, test 2: {}", test2.1.lit());
+
+  let mut input2 = parse(&fs::read_to_string("data/day20.txt").unwrap());
+  input2.1.run(&input2.0, 50);
+  assert_eq!(input2.1.lit(), 4873);
+  println!("Day 20, part 2: {}", input2.1.lit());
+}
+
+fn parse(input: &str) -> (Algorithm, Image) {
+  let (algorithm, image) = input.split_once("\n\n").unwrap();
+  (
+    Algorithm::new(algorithm.lines().collect()),
+    Image::new(image),
+  )
+}
